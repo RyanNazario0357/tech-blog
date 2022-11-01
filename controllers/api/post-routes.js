306f99a8fa15af444c90130102ca7d3f95d556b9
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-01 19:19:31 -0400

    edit post with upvote in database

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -87,3 +87,12 @@ router.post('/', withAuth, (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.put('/upvote', withAuth, (req, res) => {
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});

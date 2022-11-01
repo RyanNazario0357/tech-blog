41628c3ea54ff9fef75961a83a998b9adc7e36c8
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-01 19:17:12 -0400

    save created post to database

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -75,3 +75,15 @@ router.get('/:id', (req, res) => {
         });
 });
 
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.session.user_id
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});

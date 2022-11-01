54a57d8ff73edd8ef2f81f80b1955d3c5d09d226
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-01 18:48:53 -0400

    edited home routes and created index file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,13 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
-const withAuth = require('../utils/auth');
 
-// return all posts for dashboard from db
-router.get('/', withAuth, (req, res) => {
-    console.log(req.session);
+// return all posts for homepage from db
+router.get('/', (req, res) => {
     console.log('====================');
     Post.findAll({
-        where: {
-            user_id: req.session.user_id
-        },
         attributes: [
-            'id',
+            'id', 
             'post_url',
             'title',
             'created_at',
@@ -35,22 +30,31 @@ router.get('/', withAuth, (req, res) => {
     })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('dashboard', { posts, loggedIn: true });
+
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
 });
-router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findByPk(req.params.id, {
+
+// get single post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id 
+        },
         attributes: [
             'id',
             'post_url',
             'title',
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
+        ], 
         include: [
             {
                 model: Comment,
@@ -67,21 +71,31 @@ router.get('/edit/:id', withAuth, (req, res) => {
         ]
     })
         .then(dbPostData => {
-            if (dbPostData) {
-                const post = dbPostData.get({ plain: true });
-
-                res.render('edit-post', {
-                    post,
-                    loggedIn: true 
-                });
-            } else {
-                res.status(404).end();
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id'});
+                return;
             }
+
+            const post = dbPostData.get({ plain: true });
+
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
-            console.log(err)
+            console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
